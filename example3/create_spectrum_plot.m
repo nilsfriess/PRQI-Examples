721dff8efec8827b48e6710111256165337c6774
Nils Friess <nils.friess@gmail.com>
2023-11-29 17:09:54 +0100

    Make some adjustments for plots of example 3

--- example3/create_spectrum_plot.m
+++ example3/create_spectrum_plot.m
@@ -1,14 +1,14 @@
-function create_spectrum_plot(E_unpert, E_pert, sp_idx, tgt_idx, tgt_idx_pert)
+function create_spectrum_plot(E_unpert, E_pert, filename, sp_idx, tgt_idx, tgt_idx_pert)
 fig = figure();
 fig.Units               = 'centimeters';
 fig.Position(3)         = 18;
-fig.Position(4)         = 8;
+fig.Position(4)         = 6;
 
 orange = [0.9020, 0.6235, 0];
 blue   = [0, 0.4471, 0.6980];
 red    = [238/255, 75/255, 43/255];
 
-if nargin > 2
+if nargin > 3
     ylim([-0.1,1.1]);
     ax = gca;
     ax.XTick = -1 : 0.1 : 2;
@@ -49,7 +49,7 @@ if nargin > 2
                   'Spectrum of the perturbed problem',...
                   'Perturbed target'};
     [legend_h, object_h, plot_h, text_str] = legendflex(plots, legendData,...
-        'anchor', {'e','e'});
+        'anchor', [4 4], 'buffer', [-12 8]);
     hatchfill2(object_h(7), 'single',...
                'HatchAngle', 70,...
                'HatchDensity', 30,...
@@ -59,11 +59,14 @@ if nargin > 2
         set(object_h(i), 'interpreter', 'latex');
     end
 else
+    fig.Position(4) = 5;
+
     plot(real(E_unpert), imag(E_unpert), 'o', 'markersize', 6, 'markeredgecolor', orange, 'markerfacecolor', orange);
     hold on;
     plot(real(E_pert), imag(E_pert), '^', 'markersize', 6, 'markeredgecolor', blue, 'markerfacecolor', blue);
     legend("Spectrum of the original problem", "Spectrum of the perturbed problem", "location", "east", 'Interpreter','latex');
     xlim([-0.56, 2]);
+    ylim([-0.09,1.09])
 
     ax = gca;
     ax.XTick = -1 : 0.5 : 2;
@@ -72,7 +75,9 @@ end
 xlabel("Real",'interpreter','latex');
 ylabel("Imaginary",'interpreter','latex');
 
+set(gca,'TickLabelInterpreter','latex')
+
 box on;
-print('spectrum', '-dpng', '-r300');
+exportgraphics(fig, filename, 'ContentType', 'vector');
 end
 
--- example3/generate_results.m
+++ example3/generate_results.m
@@ -1,4 +1,6 @@
 [A,B,M,x,E,Ep] = assemble_schroedinger(107.5, 10750, true, true);
 
-create_spectrum_plot(E, Ep, 25, 24, 24);
+create_spectrum_plot(E, Ep, "spectrum.pdf")
+create_spectrum_plot(E, Ep, "spectrum-zoom.pdf", 25, 24, 24);
+
 create_table_results(A+B, M, E, x)
