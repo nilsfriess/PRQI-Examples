806bc71c336a84121cba57ba40e3b74a902eda46
Nils Friess <nils.friess@gmail.com>
2023-11-30 09:16:50 +0100

    Add variable for meshwidth

--- example3/generate_results.m
+++ example3/generate_results.m
@@ -1,4 +1,7 @@
-[A,B,M,x,E,Ep] = assemble_schroedinger(107.5, 10750, true, true);
+X = 107.5;
+h = 0.01;
+N = X * 1 / h;
+[A,B,M,x,E,Ep] = assemble_schroedinger(X, N, true, true);
 
 create_spectrum_plot(E, Ep, "spectrum.pdf")
 create_spectrum_plot(E, Ep, "spectrum-zoom.pdf", 25, 24, 24);
