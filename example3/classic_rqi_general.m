1baa4691dd1c6aed1f23f9304997369d77085c96
Nils Friess <nils.friess@gmail.com>
2023-12-01 15:25:24 +0100

    Allow to specify shift in classic_RQI

--- example3/classic_rqi_general.m
+++ example3/classic_rqi_general.m
@@ -1,4 +1,4 @@
-function [e, varargout] = classic_rqi_general(A, M, x0, tol)
+function [e, varargout] = classic_rqi_general(A, M, x0, mu, tol)
 % CLASSIC_RQI_GENERAL  Performs the classic Rayleigh Quotient
 %                      Iteration (RQI) for the eigenvalue problem
 %                      Av = \lambda Mv
@@ -7,19 +7,24 @@ function [e, varargout] = classic_rqi_general(A, M, x0, tol)
 %                                     vector x0 until the residual
 %                                     norm is less than or equal 10^-8
 %
-%   e = CLASSIC_RQI_GENERAL(A, M, x0, tol) applies RQI with the
-%                                          initial vector x0 until the
-%                                          residual norm is less than
-%                                          or equal tol
+%   e = CLASSIC_RQI_GENERAL(A, M, x0, mu, tol) applies RQI with the
+%                                              initial vector x0  and
+%                                              initial shift mu until
+%                                              the residual norm is less
+%                                              than or equal to tol
 %
 x0 = x0(:);
 v = x0 / sqrt(x0'*M*x0);
 
-e = v'*A*v; % Rayleigh quotient
+if nargin < 4
+    e = v'*A*v; % Rayleigh quotient
+else
+    e = mu;
+end
 
 res = norm((A - e*M)*v); % Residual
 
-if nargin < 4
+if nargin < 5
     tol = 1e-8;
 end
 
